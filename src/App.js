780c6c1ec01c8d0928e44ef01dcee198fcb6e7a1
Marcellino Deodatus <deodatusmarcellino@gmail.com>
2025-08-11 14:54:34 -0400

    Display all SongArtists connection

--- src/App.js
+++ src/App.js
@@ -3,23 +3,40 @@ import React, { useState, useEffect } from "react";
 function App() {
   const [songsWithArtists, setSongsWithArtists] = useState([]);
 
+  // Helper to fetch all paginated data from a DAB endpoint
+  async function fetchAll(endpoint) {
+    let all = [];
+    let url = endpoint;
+    while (url) {
+      const res = await fetch(url);
+      const data = await res.json();
+      all = all.concat(data.value || []);
+      // Remove domain from nextLink if present
+      url = data.nextLink
+        ? data.nextLink.replace(/^https?:\/\/[^/]+/, "")
+        : null;
+    }
+    return all;
+  }
+
   useEffect(() => {
-    Promise.all([
-      fetch("/data-api/rest/SongArtists").then((res) => res.json()),
-      fetch("/data-api/rest/Songs").then((res) => res.json()),
-      fetch("/data-api/rest/Artists").then((res) => res.json()),
-    ]).then(([sa, songs, artists]) => {
+    (async () => {
+      const [sa, songs, artists] = await Promise.all([
+        fetchAll("/data-api/rest/SongArtists"),
+        fetchAll("/data-api/rest/Songs"),
+        fetchAll("/data-api/rest/Artists"),
+      ]);
       const songMap = {};
       const artistMap = {};
-      (songs.value || []).forEach((s) => (songMap[s.song_id] = s.title));
-      (artists.value || []).forEach((a) => (artistMap[a.artist_id] = a.name));
+      songs.forEach((s) => (songMap[s.song_id] = s.title));
+      artists.forEach((a) => (artistMap[a.artist_id] = a.name));
 
-      const joined = (sa.value || []).map((item) => ({
+      const joined = sa.map((item) => ({
         title: songMap[item.song_id] || "Unknown Song",
         artist: artistMap[item.artist_id] || "Unknown Artist",
       }));
       setSongsWithArtists(joined);
-    });
+    })();
   }, []);
 
   return (
