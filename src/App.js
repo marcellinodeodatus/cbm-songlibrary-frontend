5699a9c03490790050e6f7f7a41a280f1dc3705f
Marcellino Deodatus <deodatusmarcellino@gmail.com>
2025-08-11 14:33:44 -0400

    Fetch all three tables separately, then join using their IDs.

--- src/App.js
+++ src/App.js
@@ -4,11 +4,22 @@ function App() {
   const [songsWithArtists, setSongsWithArtists] = useState([]);
 
   useEffect(() => {
-    fetch(
-      "/data-api/rest/SongArtists?$expand=song($select=title),artist($select=name)"
-    )
-      .then((res) => res.json())
-      .then((data) => setSongsWithArtists(data.value || []));
+    Promise.all([
+      fetch("/data-api/rest/SongArtists").then((res) => res.json()),
+      fetch("/data-api/rest/Songs").then((res) => res.json()),
+      fetch("/data-api/rest/Artists").then((res) => res.json()),
+    ]).then(([sa, songs, artists]) => {
+      const songMap = {};
+      const artistMap = {};
+      (songs.value || []).forEach((s) => (songMap[s.song_id] = s.title));
+      (artists.value || []).forEach((a) => (artistMap[a.artist_id] = a.name));
+
+      const joined = (sa.value || []).map((item) => ({
+        title: songMap[item.song_id] || "Unknown Song",
+        artist: artistMap[item.artist_id] || "Unknown Artist",
+      }));
+      setSongsWithArtists(joined);
+    });
   }, []);
 
   return (
@@ -25,14 +36,12 @@ function App() {
       }}
     >
       <h1 style={{ textAlign: "center" }}>Song Library</h1>
-
       <div style={{ margin: "2rem 0" }}>
         <h2>All Songs with Artists</h2>
         <ul>
           {songsWithArtists.map((item, idx) => (
             <li key={idx}>
-              <strong>{item.song?.title || "Unknown Song"}</strong> -{" "}
-              {item.artist?.name || "Unknown Artist"}
+              <strong>{item.title}</strong> - {item.artist}
             </li>
           ))}
         </ul>
